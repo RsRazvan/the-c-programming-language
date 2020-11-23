0dd6a98b9c4f43dabccf728aa01db8c3e7d83af7
Razvan <59976855+RsRazvan@users.noreply.github.com>
2020-11-23 16:41:55 +0200

    Create 1-04.c

---
+++ chapter_1/exercice_04/1-04.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table
+for celsius = 0, 20, ..., 300; floatinq-point version */
+
+int main(void)
+{
+    int fahr, celsius;
+    int upper, lower, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+// Header before the table
+
+    printf("Celsius \t Fahrenheid\n\n");
+
+    celsius = lower;
+    while (celsius <= upper)
+    {
+        fahr = (5.0 / 9.0 * celsius) + 32.0;
+        printf(" %3.0f \t\t %6.0f\n", celsius, fahr);
+        celsius += step;
+    }
+}
