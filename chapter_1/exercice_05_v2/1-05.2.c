edbb86443b62a425559ae9a86c8feb1bc37a1399
Razvan <59976855+RsRazvan@users.noreply.github.com>
2020-11-25 23:17:54 +0200

    Create 1-05.2.c

---
+++ chapter_1/exercice_05_v2/1-05.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int fahr;
+
+	printf("Fahrenheit \t Celsius\n\n");
+
+	for (fahr = 300; fahr >= 0; fahr -= 20)
+	{
+		printf("%3d \t\t %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+	}
+
+}
