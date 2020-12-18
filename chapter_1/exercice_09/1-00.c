6c882e29c740aacae7029c57568370c5d68688fd
Razvan <59976855+RsRazvan@users.noreply.github.com>
2020-12-18 14:10:38 +0200

    Create 1-00.c

---
+++ chapter_1/exercice_09/1-00.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*This program copy its input to its output and reaplce each string of one or more blanks by a single blank*/
+
+int main(void)
+{
+	char c, aux; 
+
+	putchar(aux = getchar());
+	
+	while ((c = getchar()) != EOF)
+	{
+	
+		if (c != ' ' || aux != ' ')
+		{
+			putchar(c);
+		}
+		
+		aux = c;
+	}
+
+}
